601b96e4b38795c6284c025d37799fcff8375e48
Marty Fitzer <martyfitzer@gmail.com>
2020-07-14 17:08:20 -0500

    Adds basic math data filters

---
+++ Assets/Scripts/DataFilters/MultiAudioFilter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public abstract class MultiAudioFilter : DataFilter
+{
+    [SerializeField]
+    protected AudioVisualizationController filterDataSource;
+
+    protected VisualizationData filterData { get; private set; }
+
+    protected bool filterDataSet
+    {
+        get
+        {
+            return filterData.audioBands != null;
+        }
+    }
+
+    protected virtual void OnEnable()
+    {
+        filterDataSource.setVisualizationListener(setFilterData);
+        filterDataSource.enable();
+    }
+
+    protected virtual void OnDisable()
+    {
+        filterDataSource.disable();
+    }
+
+    private void setFilterData(VisualizationData data)
+    {
+        filterData = data;
+    }
+}
