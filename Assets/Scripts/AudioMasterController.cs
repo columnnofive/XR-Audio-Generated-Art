de6491bb233bdbe62ca5d216489cae6e52881184
Marty Fitzer <martyfitzer@gmail.com>
2020-07-16 10:37:15 -0500

    Configures audio to be decoded as ambisonic audio using OculusSpatializer

--- Assets/Scripts/AudioMasterController.cs
+++ Assets/Scripts/AudioMasterController.cs
@@ -2,27 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
 public class AudioMasterController : MonoBehaviour
 {
-    private AudioSource audioSource;
+    [SerializeField]
+    private AudioSource[] audioSources;
 
     [Range(0, 1), SerializeField]
     private float clipTime = 0f;
     
     private float previousClipTime = 0f;
 
-    private void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
-
     private void updateClipTime()
     {
-        if (clipTime != previousClipTime) //Clip time was set manually
-            audioSource.time = audioSource.clip.length * clipTime;
-        else //set clipTime to follow audioSource.time
-            clipTime = audioSource.time / (audioSource.clip.length - 1);
+        foreach (AudioSource audioSource in audioSources)
+        {
+            if (clipTime != previousClipTime) //Clip time was set manually
+                audioSource.time = audioSource.clip.length * clipTime;
+            else //set clipTime to follow audioSource.time
+                clipTime = audioSource.time / (audioSource.clip.length - 1);
+        }
 
         previousClipTime = clipTime;
     }
