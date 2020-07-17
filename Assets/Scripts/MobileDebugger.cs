be8e9e160f28b4f6a8b338b611ea8435d74dc838
Marty Fitzer <martyfitzer@gmail.com>
2020-07-17 10:39:18 -0500

    Creates mobile debugger

---
+++ Assets/Scripts/MobileDebugger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MobileDebugger : MonoBehaviour
+{
+    [SerializeField]
+    private Text debugText;
+
+    private void Start()
+    {
+        debugText.text = ""; //Clear placeholder text
+    }
+
+    private void OnEnable()
+    {
+        Application.logMessageReceivedThreaded += handleLogMessage;
+    }
+
+    private void OnDisable()
+    {
+        Application.logMessageReceivedThreaded -= handleLogMessage;
+    }
+
+    private void handleLogMessage(string message, string stackTrace, LogType type)
+    {
+        debugText.text += message + "\n";
+    }
+}
