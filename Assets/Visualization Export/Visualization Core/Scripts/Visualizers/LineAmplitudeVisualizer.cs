e63f91753ae26fd5867de129caa18d4e6c9f8509
Marty Fitzer <martyfitzer@gmail.com>
2020-07-23 10:23:13 -0500

    Refactors visualizer scripts
    - Refactors line visualizers to use central, serializable class
    - Refactors position visualizers to use central, serializable class
    - Adds more configuration to position visualizer to control how movement is constrained to the movement boundaries

--- Assets/Visualization Export/Visualization Core/Scripts/Visualizers/LineAmplitudeVisualizer.cs
+++ Assets/Visualization Export/Visualization Core/Scripts/Visualizers/LineAmplitudeVisualizer.cs
@@ -5,87 +5,38 @@ using UnityEngine;
 [RequireComponent(typeof(TrailRenderer))]
 public class LineAmplitudeVisualizer : AudioVisualizer
 {
-    private TrailRenderer trailRenderer;
-
-    [Header("Width")]
-
     [SerializeField]
-    private float minWidth = 0.5f;
-
-    [SerializeField]
-    private float maxWidth = 1f;
-
-    [Header("Color")]
-
-    [SerializeField, Range(0, 1)]
-    private float colorChangeThreshold = 0.5f;
-
-    [SerializeField]
-    private float colorChangeSpeed = 1f;
-
-    [SerializeField, Range(0, 1)]
-    private float hueMin = 0f;
-
-    [SerializeField, Range(0, 1)]
-    private float hueMax = 1f;
-
-    [SerializeField]
-    private float emissionIntensity = 1f;
-
-    [SerializeField, ShaderProperty(ShaderPropertyType.Color)]
-    private ShaderPropertyField shaderColorField = new ShaderPropertyField
-    {
-        fieldName = "_EmissionColor"
-    };
-
-    private Color targetColor;
+    private LineVisualizer lineVisualizer;
 
     private void OnValidate()
     {
-        if (!trailRenderer)
-            trailRenderer = GetComponent<TrailRenderer>();
+        if (!lineVisualizer.trailRenderer)
+            lineVisualizer.trailRenderer = GetComponent<TrailRenderer>();
 
-        if (trailRenderer)
+        if (lineVisualizer.trailRenderer)
         {
-            if (trailRenderer.sharedMaterial)
+            if (lineVisualizer.trailRenderer.sharedMaterial)
             {
-                shaderColorField.shader = trailRenderer.sharedMaterial.shader;
+                lineVisualizer.shaderColorField.shader = lineVisualizer.trailRenderer.sharedMaterial.shader;
             }
             else
             {
-                shaderColorField.shader = null;
+                lineVisualizer.shaderColorField.shader = null;
             }
         }
     }
 
     private void Start()
     {
-        trailRenderer = GetComponent<TrailRenderer>();
+        lineVisualizer.trailRenderer = GetComponent<TrailRenderer>();
     }
 
     protected override void visualizeData(VisualizationData data)
     {
-        setWidth(data.amplitude);
-        setColor(data.amplitude);
-    }
+        if (lineVisualizer.controlWidth)
+            lineVisualizer.setWidth(data.amplitude, visualizerScaleFactor);
 
-    private void setWidth(float amplitude)
-    {
-        float widthInterpolation = (maxWidth - minWidth) * visualizerScaleFactor * amplitude;
-        trailRenderer.widthMultiplier = minWidth + widthInterpolation;
-    }
-
-    //Randomly set emission color based on audio amplitude.
-    private void setColor(float amplitude)
-    {
-        // TIMER to determine when to change
-        if (amplitude > colorChangeThreshold)
-            targetColor = Random.ColorHSV(hueMin, hueMax, 1f, 1f, 0, 1, 1, 1) * emissionIntensity;
-
-        //Lerp to target color
-        Color currentColor = trailRenderer.material.GetColor(shaderColorField.fieldName);
-        Color color = Color.Lerp(currentColor, targetColor, Time.deltaTime * colorChangeSpeed);
-
-        trailRenderer.material.SetColor(shaderColorField.fieldName, color);
+        if (lineVisualizer.controlColor)
+            lineVisualizer.setColor(data.amplitude);
     }
 }
--- Assets/Visualization Export/Visualization Core/Scripts/Visualizers/LineBandVisualizer.cs
+++ Assets/Visualization Export/Visualization Core/Scripts/Visualizers/LineBandVisualizer.cs
@@ -5,88 +5,38 @@ using UnityEngine;
 [RequireComponent(typeof(TrailRenderer))]
 public class LineBandVisualizer : AudioBandVisualizer
 {
-    [SerializeField, HideInInspector]
-    private TrailRenderer trailRenderer;
-
-    [Header("Width")]
-
     [SerializeField]
-    private float minWidth = 0.5f;
-
-    [SerializeField]
-    private float maxWidth = 1f;
-
-    [Header("Color")]
-
-    [SerializeField, Range(0, 1)]
-    private float colorChangeThreshold = 0.5f;
-
-    [SerializeField]
-    private float colorChangeSpeed = 1f;
-
-    [SerializeField, Range(0, 1)]
-    private float hueMin = 0f;
-
-    [SerializeField, Range(0, 1)]
-    private float hueMax = 1f;
-
-    [SerializeField]
-    private float emissionIntensity = 1f;
-
-    [SerializeField, ShaderProperty(ShaderPropertyType.Color)]
-    private ShaderPropertyField shaderColorField = new ShaderPropertyField
-    {
-        fieldName = "_EmissionColor"
-    };
-
-    private Color targetColor;
+    private LineVisualizer lineVisualizer;
 
     private void OnValidate()
     {
-        if (!trailRenderer)
-            trailRenderer = GetComponent<TrailRenderer>();
+        if (!lineVisualizer.trailRenderer)
+            lineVisualizer.trailRenderer = GetComponent<TrailRenderer>();
 
-        if (trailRenderer)
+        if (lineVisualizer.trailRenderer)
         {
-            if (trailRenderer.sharedMaterial)
+            if (lineVisualizer.trailRenderer.sharedMaterial)
             {
-                shaderColorField.shader = trailRenderer.sharedMaterial.shader;
+                lineVisualizer.shaderColorField.shader = lineVisualizer.trailRenderer.sharedMaterial.shader;
             }
             else
             {
-                shaderColorField.shader = null;
+                lineVisualizer.shaderColorField.shader = null;
             }
         }
     }
 
     private void Start()
     {
-        trailRenderer = GetComponent<TrailRenderer>();
+        lineVisualizer.trailRenderer = GetComponent<TrailRenderer>();
     }
 
     protected override void visualizeData(VisualizationData data)
     {
-        setWidth(data.audioBands[band]);
-        setColor(data.audioBands[band]);
-    }
+        if (lineVisualizer.controlWidth)
+            lineVisualizer.setWidth(data.audioBands[band], visualizerScaleFactor);
 
-    private void setWidth(float bandAmplitude)
-    {
-        float widthInterpolation = (maxWidth - minWidth) * visualizerScaleFactor * bandAmplitude;
-        trailRenderer.widthMultiplier = minWidth + widthInterpolation;
-    }
-
-    //Randomly set emission color based on audio band amplitude.
-    private void setColor(float bandAmplitude)
-    {
-        // TIMER to determine when to change
-        if (bandAmplitude > colorChangeThreshold)
-            targetColor = Random.ColorHSV(hueMin, hueMax, 1f, 1f, 0, 1, 1, 1) * emissionIntensity;
-
-        //Lerp to target color
-        Color currentColor = trailRenderer.material.GetColor(shaderColorField.fieldName);
-        Color color = Color.Lerp(currentColor, targetColor, Time.deltaTime * colorChangeSpeed);
-
-        trailRenderer.material.SetColor(shaderColorField.fieldName, color);
+        if (lineVisualizer.controlColor)
+            lineVisualizer.setColor(data.audioBands[band]);
     }
 }
