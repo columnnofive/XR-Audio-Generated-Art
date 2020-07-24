9fe867546ad27d9c6344c9282df8d5710027c397
Marty Fitzer <martyfitzer@gmail.com>
2020-07-24 08:21:28 -0500

    Adds timeline functionality
    - Adds timeline with different events to control the visualization based on predefined conditions

---
+++ Assets/Visualization Export/Visualization Core/Scripts/Timeline/Conditions/EventCondition.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class EventCondition : TimelineCondition
+{
+    [SerializeField, Tooltip("This event must fire before the condition is met.")]
+    private TimelineEvent eventDependency;
+
+    [SerializeField, ReadOnlyField]
+    private bool dependencyEventFired = false;
+
+    public override bool isMet()
+    {
+        dependencyEventFired = eventDependency.fired;
+        return dependencyEventFired;
+    }
+}
