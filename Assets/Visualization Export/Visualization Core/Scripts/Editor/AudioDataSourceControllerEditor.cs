757ecf05b231a45f19e9bf948b8e5355c34631d3
Marty Fitzer <martyfitzer@gmail.com>
2020-07-27 16:27:56 -0500

    Adds AudioDataSourceController
    - Adds script to allow an AudioSource clip to be set to microphone input for realtime spectrum analysis

---
+++ Assets/Visualization Export/Visualization Core/Scripts/Editor/AudioDataSourceControllerEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AudioDataSourceController))]
+public class AudioDataSourceControllerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //Only allow settings to be adjusted when not in playmode
+        bool allowEdits = !EditorApplication.isPlaying && !EditorApplication.isPaused;
+
+        if (!allowEdits) //Disable
+            GUI.enabled = false;
+
+        AudioDataSourceController controller = (AudioDataSourceController)target;
+
+        SerializedProperty audioDataSourceProp = serializedObject.FindProperty("audioDataSource");
+        EditorGUILayout.PropertyField(audioDataSourceProp);
+
+        var audioDataSource = (AudioDataSourceController.AudioDataSource)audioDataSourceProp.enumValueIndex;
+        if (audioDataSource == AudioDataSourceController.AudioDataSource.Microphone)
+        {
+            SerializedProperty micDeviceProp = serializedObject.FindProperty("micDevice");
+            EditorGUILayout.PropertyField(micDeviceProp);
+        }
+
+        if (!allowEdits) //reenable gui
+            GUI.enabled = true;
+        else //Save any changes made to serialized properties
+            serializedObject.ApplyModifiedProperties();
+    }
+}
