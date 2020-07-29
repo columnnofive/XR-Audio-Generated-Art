3180c279e29f755516931f74fe67bf0497b4a228
Marty Fitzer <martyfitzer@gmail.com>
2020-07-29 10:23:07 -0500

    Adds basic graphic score interpreter and visualizer
    - Adds ability to interpret graphic score as displacement values
    - Adds graphic score pulser that pulses a shader float value based on the displacement values

---
+++ Assets/Visualization Export/Visualization Core/Scripts/GraphicScoreInterpreter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public static class GraphicScoreInterpreter
+{
+    private readonly static float whiteGrayscale = 1f;
+
+    public static float[] getDisplacementValues(Texture2D tex)
+    {
+        float[] displacementValues = new float[tex.width];
+
+        Color[][] pixels = tex.GetFormattedPixels();
+        
+        for (int i = 0; i < tex.width; i++)
+        {
+            int whitePixels = 0;
+            
+            for (int j = 0; j < tex.height; j++)
+            {
+                float pixelGrayscale = pixels[j][i].grayscale;
+
+                if (pixelGrayscale == whiteGrayscale)
+                    whitePixels++;
+            }
+
+            displacementValues[i] = 1f - ((float)whitePixels / tex.height);
+        }
+
+        return displacementValues;
+    }
+}
---
+++ Assets/Visualization Export/Visualization Core/Scripts/Texture2DExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Texture2DExtensions
+{
+    /// <summary>
+    /// Returns array of pixels colors in format, Color[height][width].
+    /// </summary>
+    public static Color[][] GetFormattedPixels(this Texture2D tex)
+    {
+        Color[][] pixels = new Color[tex.height][];
+
+        Color[] unformattedPixels = tex.GetPixels();
+
+        int pixelIndex = 0;
+        for (int i = 0; i < tex.height; i++)
+        {
+            pixels[i] = new Color[tex.width];
+
+            for (int j = 0; j < tex.width; j++, pixelIndex++)
+            {
+                pixels[i][j] = unformattedPixels[pixelIndex];
+            }
+        }
+
+        return pixels;
+    }
+}
