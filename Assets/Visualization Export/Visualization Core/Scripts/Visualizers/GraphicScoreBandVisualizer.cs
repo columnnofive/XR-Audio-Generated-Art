dcd8a31bc460a6076da6083d83317056a1ace639
Marty Fitzer <martyfitzer@gmail.com>
2020-08-05 15:07:22 -0500

    Optimizes graphic stream visualizers
    - Also adds Graphy debugging package for FPS tracking

--- Assets/Visualization Export/Visualization Core/Scripts/Visualizers/GraphicScoreBandVisualizer.cs
+++ Assets/Visualization Export/Visualization Core/Scripts/Visualizers/GraphicScoreBandVisualizer.cs
@@ -8,31 +8,7 @@ public class GraphicScoreBandVisualizer : AudioBandVisualizer
     protected override void OnValidate()
     {
         base.OnValidate();
-
-        if (!visualizer.rend || visualizer.rend.transform != transform)
-        {
-            visualizer.rend = GetComponent<Renderer>();
-        }
-
-        if (visualizer.rend && visualizer.rend.sharedMaterial)
-        {
-            if (visualizer.rend.sharedMaterial != visualizer.material) //Material changed
-            {
-                visualizer.material = visualizer.rend.sharedMaterial;
-                visualizer.displacementTexField.shader = visualizer.material.shader;
-                visualizer.displacementAmountField.shader = visualizer.material.shader;
-            }
-        }
-        else
-        {
-            visualizer.material = null;
-            visualizer.displacementTexField.shader = null;
-            visualizer.displacementAmountField.shader = null;
-        }
-
-        //Constrain to positive value
-        if (visualizer.displacementStreamSize < 0)
-            visualizer.displacementStreamSize = 0;
+        visualizer.OnValidate(gameObject);
     }
 
     private void Start()
